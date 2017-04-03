6cc68c126180b631d94e68d217c3b81a57a23da1
hg <hannesglav@users.noreply.github.com>
2017-04-03 18:25:32 +0200

    myLecture4: AutoValue Klasse Message fertig

--- myLecture4/src/main/java/at/technikumwien/data/Message.java
+++ myLecture4/src/main/java/at/technikumwien/data/Message.java
@@ -1,11 +1,17 @@
 package at.technikumwien.data;
 
+import com.google.auto.value.AutoValue;
+
 /**
  * Created by FH on 03.04.2017.
  */
 
 @AutoValue
-public class Message {
+public abstract class Message {
+    public abstract String title();
+    public abstract String message();
 
-
+    public static Message create(String title, String message) {
+        return new AutoValue_Message(title,message);
+    }
 }
